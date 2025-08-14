5ca95f15af59c755b719e9f33d76c91f73d73fbc
warabisan <amimotoryouta@gmail.com>
2025-08-14 14:37:30 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import pathlib
+import discord
+from discord.ext import commands
+
+TOKEN = os.getenv("TOKEN")
+if not TOKEN:
+    raise ValueError("TOKENが設定されていません（Renderの環境変数にTOKENを追加してください）")
+
+bot = commands.Bot(command_prefix=">")
+
+@bot.event
+async def on_ready():
+    print(f"ログインが完了しました：{bot.user} (ID：{bot.user.id})")
+
+    cog_path = pathlib.Path("cogs")
+    for file in cog_path.glob("*.py"):
+        if file.name.startswith("_"):
+            continue
+        module = f"cogs.{file.stem}"
+        try:
+            await bot.load_extension(module)
+            print(f"読み込み成功：{module}")
+        except Exception as e:
+            print(f"読み込み失敗：{module} / {e}")
+
+bot.run(TOKEN)
